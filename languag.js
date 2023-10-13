cc0808456560ad300fb24fef843c4980b0f37fb7
Bogdan1706 <bogdan.v.n@yandex.ru>
2023-10-13 06:49:30 +0300

    fixed the operation of modal windows on the login and registration buttons join

--- languag.js
+++ languag.js
@@ -4,18 +4,47 @@ const triangleCn = document.getElementById('triangle-cn')
 const itemRu = document.getElementById('item-ru')
 const itemEn = document.getElementById('item-en')
 const itemCn = document.getElementById('item-cn')
+triangleRu.style.opacity = "1"
+// triangleEn.style.opacity = "0"
+// triangleCn.style.opacity = "0"
 
 itemRu.addEventListener('click', () => {
-   if (triangleRu.classList == 'header-languageselection__icon-down'){
+   if (triangleRu.style.opacity == "0") {
+      if (itemEn.classList.contains('language-item--active')){
+         itemRu.classList.add('language-item--active')
+         itemRu.classList.remove('language-item--open-ru')
+         triangleRu.style.opacity = "1"
+         itemEn.classList.remove('language-item--active')
+         itemEn.classList.add('language-item--hide')
+         triangleEn.style.opacity = "0"
+         itemCn.classList.remove('language-item--open-cn')
+         itemCn.classList.add('language-item--hide')
+         triangleEn.style.opacity = "0"
+         triangleRu.classList.add('header-languageselection__icon-down')
+         triangleRu.classList.remove('header-languageselection__icon-up')
+      } else {
+         itemRu.classList.add('language-item--active')
+         itemRu.classList.remove('language-item--open-ru')
+         triangleRu.style.opacity = "1"
+         itemEn.classList.remove('language-item--open-en')
+         itemEn.classList.add('language-item--hide')
+         triangleEn.style.opacity = "0"
+         itemCn.classList.remove('language-item--active')
+         itemCn.classList.add('language-item--hide')
+         triangleEn.style.opacity = "0"
+         triangleRu.classList.add('header-languageselection__icon-down')
+         triangleRu.classList.remove('header-languageselection__icon-up')
+      }
+   }else if ((triangleRu.style.opacity == "1")&&(triangleRu.classList.contains('header-languageselection__icon-down'))){
       itemEn.classList.remove('language-item--hide')
-      itemEn.classList.add('language-item--open-en')
+      itemEn.classList.toggle('language-item--open-up')
+      triangleEn.style.opacity = "0"
       itemCn.classList.remove('language-item--hide')
-      itemCn.classList.add('language-item--open-cn')
+      itemCn.classList.add('language-item--open-dw')
+      triangleCn.style.opacity = "0"
       triangleRu.classList.remove('header-languageselection__icon-down')
       triangleRu.classList.add('header-languageselection__icon-up')
-      triangleEn.style.opacity = "0"
-      triangleCn.style.opacity = "0"
-   } else {
+   } else  if ((triangleRu.style.opacity == "1")&&(triangleRu.classList.contains('header-languageselection__icon-up'))) {
       itemEn.classList.remove('language-item--open-en')
       itemEn.classList.add('language-item--hide')
       itemCn.classList.remove('language-item--open-cn')
@@ -25,20 +54,84 @@ itemRu.addEventListener('click', () => {
    }
 })
 
-// itemEn.addEventListener('click', () => {
-//    itemRu.classList.remove('language-item--active')
-//    itemRu.classList.add('language-item--hide-ru')
+itemEn.addEventListener('click', () => {
+   if (triangleEn.style.opacity == "0") {
+      itemRu.classList.remove('language-item--active')
+      itemRu.classList.add('language-item--hide')
+      itemEn.classList.remove('language-item--open-en')
+      itemEn.classList.add('language-item--active')
+      triangleEn.style.opacity = "1"
+      itemCn.classList.remove('language-item--open-cn')
+      itemCn.classList.add('language-item--hide')
+      triangleEn.classList.remove('header-languageselection__icon-up')
+      triangleEn.classList.add('header-languageselection__icon-down')
+   }else if ((triangleEn.style.opacity == "1") && (triangleEn.classList.contains('header-languageselection__icon-down'))) {
+      itemRu.classList.remove('language-item--hide')
+      itemRu.classList.add('language-item--open-ru')
+      triangleRu.style.opacity = "0"
+      itemCn.classList.remove('language-item--hide')
+      itemCn.classList.add('language-item--open-cn')
+      triangleCn.style.opacity = "0"
+      triangleEn.classList.remove('header-languageselection__icon-down')
+      triangleEn.classList.add('header-languageselection__icon-up')
+   }else if ((triangleEn.style.opacity == "1") && (triangleEn.classList.contains('header-languageselection__icon-up'))) {
+      itemRu.classList.add('language-item--hide')
+      itemRu.classList.remove('language-item--open-ru')
+      triangleRu.style.opacity = "0"
+      itemCn.classList.add('language-item--hide')
+      itemCn.classList.remove('language-item--open-cn')
+      triangleCn.style.opacity = "0"
+      triangleEn.classList.add('header-languageselection__icon-down')
+      triangleEn.classList.remove('header-languageselection__icon-up')
+   }
+})
 
-//    itemEn.classList.remove('language-item--open-en')
-//    itemEn.classList.add('language-item--active')
-  
-//    triangleEn.style.opacity = "1"
-
-   //itemRu.classList.add('language-item--hide-en')
-   // itemCn.classList.remove('language-item--open-cn')
-   // itemCn.classList.add('language-item--hide-cn')
-   // triangle.classList.remove('header-languageselection__icon-up')
-   // triangle.classList.add('header-languageselection__icon-down')
+itemCn.addEventListener('click', () => {
+   if (triangleCn.style.opacity == "0") {
+      if (itemRu.classList.contains('language-item--active')){
+         itemRu.classList.remove('language-item--active')
+         itemRu.classList.add('language-item--hide')
+         triangleRu.style.opacity = "0"
+         itemEn.classList.remove('language-item--open-en')
+         itemEn.classList.add('language-item--hide')
+         triangleEn.style.opacity = "0"
+         itemCn.classList.remove('language-item--open-cn')
+         itemCn.classList.add('language-item--active')
+         triangleCn.style.opacity = "1"
+         triangleCn.classList.remove('header-languageselection__icon-up')
+         triangleCn.classList.add('header-languageselection__icon-down')
+      }else {
+         itemRu.classList.remove('language-item--open-ru')
+         itemRu.classList.add('language-item--hide')
+         triangleRu.style.opacity = "0"
+         itemEn.classList.remove('language-item--active')
+         itemEn.classList.add('language-item--hide')
+         triangleEn.style.opacity = "0"
+         itemCn.classList.remove('language-item--open-cn')
+         itemCn.classList.add('language-item--active')
+         triangleCn.style.opacity = "1"
+         triangleCn.classList.remove('header-languageselection__icon-up')
+         triangleCn.classList.add('header-languageselection__icon-down')
+      }
+   }else if ((triangleCn.style.opacity == "1") && (triangleCn.classList == 'header-languageselection__icon-down')) {
+      itemRu.classList.remove('language-item--hide')
+      itemRu.classList.add('language-item--open-ru')
+      triangleRu.style.opacity = "0"
+      itemEn.classList.remove('language-item--hide')
+      itemEn.classList.add('language-item--open-cn')
+      triangleEn.style.opacity = "0"
+      triangleCn.classList.remove('header-languageselection__icon-down')
+      triangleCn.classList.add('header-languageselection__icon-up')
+   }else if ((triangleCn.style.opacity == "1") && (triangleCn.classList.contains('header-languageselection__icon-up'))) {
+      itemRu.classList.add('language-item--hide')
+      itemRu.classList.remove('language-item--open-ru')
+      triangleRu.style.opacity = "0"
+      itemEn.classList.remove('language-item--open-cn')
+      itemEn.classList.add('language-item--hide')
+      triangleEn.style.opacity = "0"
+      triangleCn.classList.remove('header-languageselection__icon-up')
+      triangleCn.classList.add('header-languageselection__icon-down')
+   }
 })
 
 
