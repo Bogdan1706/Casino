17fd013cc40e11593ee9c80e4c894c991d226d00
Bogdan1706 <bogdan.v.n@yandex.ru>
2023-10-18 18:02:51 +0300

    started working with adding messages

---
+++ messages.js
@@ -0,0 +1,11 @@
+const linkLeft = document.querySelector('#linkleft')
+const linkRight = document.querySelector('#linkright')
+slider = ["./images/partners/logo/1.png","./images/partners/logo/2.png","./images/partners/logo/3.png","./images/partners/logo/4.png","./images/partners/logo/5.png","./images/partners/logo/6.png"]
+
+linkLeft.addEventListener('click', () => {
+   
+})
+
+linkRight.addEventListener('click', () => {
+
+})
